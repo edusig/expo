c79c90215f3601b32f70c5919f8c7faaaf0aa75d
andy <40680668+ajsmth@users.noreply.github.com>
2021-08-23 11:34:22 -0700

    add build script to metro-config

--- packages/expo-stories/metro-config.js
+++ packages/expo-stories/metro-config.js
@@ -1,12 +1,26 @@
+const path = require('path');
+
+const { buildAsync } = require('./build/cli/commands/build');
 const { initAsync } = require('./build/cli/commands/init');
 const { getStoriesDir, getStoriesFile } = require('./build/cli/shared');
 
 async function withExpoStories(metroConfig) {
+  const { projectRoot } = metroConfig;
+
+  const pkg = require(path.resolve(projectRoot, 'package.json'));
+
+  const storyConfig = pkg.expoStories || {
+    projectRoot,
+    watchRoot: projectRoot,
+  };
+
   await initAsync({
     projectRoot: metroConfig.projectRoot,
   });
 
-  // TODO - add build async
+  await buildAsync(storyConfig);
+
+  throw new Error('123')
 
   const storiesDir = getStoriesDir(metroConfig);
   const storyFile = getStoriesFile(metroConfig);
